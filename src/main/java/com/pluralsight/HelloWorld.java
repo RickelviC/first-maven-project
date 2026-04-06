8db172344c690897eea5396d6e7a20739873cf55
RickelviC <Cruzrickelvi@gmail.com>
2026-04-06 15:11:44 -0400

    added a message about potatoes and more testing lines

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -3,7 +3,9 @@ package com.pluralsight;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello I am Ricky");
-        System.out.println();
+        System.out.println("this is for the second test");
+        System.out.println("just making sure");
+        System.out.println("i love potato");
 
 
     }
